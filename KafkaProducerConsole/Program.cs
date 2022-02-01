76f6df7f11b0261b8759f27f680a176af11d206b
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2022-02-01 19:36:54 +0300

    feat(producer): base producer

--- KafkaProducerConsole/Program.cs
+++ KafkaProducerConsole/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Starting Kafka producer");
+
+const string message = "IB's first Kafka message";
+
+Console.WriteLine($"We're going to produce {message}");
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+// If serializers are not specified, default serializers from
+// `Confluent.Kafka.Serializers` will be automatically used where
+// available. Note: by default strings are encoded as UTF8.
+using (var p = new ProducerBuilder<Null, string>(config).Build())
+{
+    try
+    {
+        var dr = await p.ProduceAsync("ibTestTopic", new Message<Null, string> { Value = message });
+        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+    }
+    catch (ProduceException<Null, string> e)
+    {
+        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+    }
+}
